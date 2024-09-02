d8e141ff0cbd345c919dafbf0054f4d71b882b80
sawadogo boureiman <sawadogoboure66@gmail.com>
2024-09-02 09:56:33 +0000

    2nd commit

--- src/main/java/recuperer/donnees/codeBarre/Security/AppUserDetailsService.java
+++ src/main/java/recuperer/donnees/codeBarre/Security/AppUserDetailsService.java
@@ -1,34 +1,34 @@
-//package recuperer.donnees.codeBarre.Security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//import recuperer.donnees.codeBarre.Entity.Users;
-//import recuperer.donnees.codeBarre.Service.UsersService;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//
-//@Service
-//public class AppUserDetailsService implements UserDetailsService {
-//	  @Autowired
-//	private UsersService usersService;
-//
-//
-//	public AppUserDetailsService(UsersService usersService) {
-//		this.usersService = usersService;
-//	}
-//
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		Users utilisateurs = usersService.getUserByUsername(username);
-//		Collection<GrantedAuthority> authorities = new ArrayList<>();
-//		authorities.add(new SimpleGrantedAuthority(utilisateurs.getRole()));
-//		return new User(utilisateurs.getUsername(), utilisateurs.getPassword(), authorities);
-//	}
-//}
+package recuperer.donnees.codeBarre.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import recuperer.donnees.codeBarre.Entity.Users;
+import recuperer.donnees.codeBarre.Service.UsersService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+	  @Autowired
+	private UsersService usersService;
+
+
+	public AppUserDetailsService(UsersService usersService) {
+		this.usersService = usersService;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Users utilisateurs = usersService.getUserByUsername(username);
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(utilisateurs.getRole()));
+		return new User(utilisateurs.getUsername(), utilisateurs.getPassword(), authorities);
+	}
+}
--- src/main/java/recuperer/donnees/codeBarre/Security/SecurityConfig.java
+++ src/main/java/recuperer/donnees/codeBarre/Security/SecurityConfig.java
@@ -1,71 +1,71 @@
-//package recuperer.donnees.codeBarre.Security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import recuperer.donnees.codeBarre.Service.UsersService;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//	@Autowired
-//	private UsersService usersService;
-//
-//	@Autowired
-//	private AppUserDetailsService appUserDetailsService;
-//
-//	@Bean
-//	public DaoAuthenticationProvider authenticationProvider(){
-//		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-//		auth.setUserDetailsService(appUserDetailsService);
-//		return auth;
-//	}
-//
-//	@Bean
-//	protected SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
-//
-//
-//		http.authenticationProvider(authenticationProvider()); //fais partie des config de la fonction du haut
-//
-//		http.csrf(csrf -> csrf.disable());
-//		http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
-//		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//		//http.formLogin();
-//		http.authorizeHttpRequests(auth -> auth.requestMatchers("/authenticate/**").permitAll()
-//				.requestMatchers("/utilisateur/connexion/ghx").permitAll()
-//				.requestMatchers("/login/").permitAll()
-//				.anyRequest().authenticated());
-//		http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//		return http.build();
-//	}
-//
-//	@Bean
-//	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authConfiguration) throws Exception {
-//		return authConfiguration.getAuthenticationManager();
-//	}
-//
-//	@Bean
-//	CorsConfigurationSource corsConfigurationSource() {
-//		CorsConfiguration configuration = new CorsConfiguration();
-//		configuration.setAllowedOrigins(Arrays.asList("*"));
-//		configuration.setAllowedMethods(Arrays.asList("*"));
-//		configuration.setAllowedHeaders(Arrays.asList("*"));
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/", configuration);
-//		return source;
-//	}
-//
-//}
+package recuperer.donnees.codeBarre.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import recuperer.donnees.codeBarre.Service.UsersService;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+	@Autowired
+	private UsersService usersService;
+
+	@Autowired
+	private AppUserDetailsService appUserDetailsService;
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider(){
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(appUserDetailsService);
+		return auth;
+	}
+
+	@Bean
+	protected SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
+
+
+		http.authenticationProvider(authenticationProvider()); //fais partie des config de la fonction du haut
+
+		http.csrf(csrf -> csrf.disable());
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		//http.formLogin();
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/authenticate/**").permitAll()
+				.requestMatchers("/utilisateur/connexion/ghx").permitAll()
+				.requestMatchers("/login/").permitAll()
+				.anyRequest().authenticated());
+		http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authConfiguration) throws Exception {
+		return authConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/", configuration);
+		return source;
+	}
+
+}
